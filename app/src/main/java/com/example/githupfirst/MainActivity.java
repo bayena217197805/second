77996feaedcd43c0dc49a7240159c2887e32522d
bayena <1001051099@tzfonet.org.il>
2024-09-18 09:42:27 +0300

    hiuyhiuh

--- app/src/main/java/com/example/githupfirst/MainActivity.java
+++ app/src/main/java/com/example/githupfirst/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.githupfirst;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn1,btn2;
+    private TextView textView;
+    boolean b=false;
+    int counter=0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1=findViewById(R.id.button1);
+        btn2=findViewById(R.id.buttone2);
+        textView=findViewById(R.id.textview);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    while (b != true) {
+                        textView.setText(counter + "");
+
+                        counter++;
+                    }
+
+
+                }catch(Exception e){
+            }}
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                b=true;
+            }
+        });
+
     }
+
 }
